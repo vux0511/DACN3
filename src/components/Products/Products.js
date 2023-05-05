b4824e51b029b39365c32e74ef52c0e566906c68
vux <vux.0511@gmail.com>
2023-05-05 16:01:27 +0700

    category logic

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,32 +1,64 @@
 import "./Products.scss";
 import Product from "./Product/Product";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import { FiChevronDown } from "react-icons/fi";
 
 function Products({ tittle_header }) {
-    const [clicked, setClicked] = useState(false);
+    const [activeCategoryId, setActiveCategoryId] = useState("allCategory");
 
-    const handleClick = () => {
-        setClicked(!clicked);
+    const handleClick = (id) => {
+        setActiveCategoryId(id);
     };
 
     return (
         <>
             <h3 className="sec-heading">{tittle_header}</h3>
             <div className="category">
-                <button className="all-prod">Tất Cả Sản Phẩm</button>
                 <button
-                    onClick={handleClick}
-                    className={`unclicked ${clicked ? "clicked" : ""}`}
+                    id="allCategory"
+                    className={
+                        activeCategoryId === "allCategory" ? "active" : ""
+                    }
+                    onClick={() => handleClick("allCategory")}
+                >
+                    Tất Cả Sản Phẩm
+                </button>
+                <button
+                    id="button1"
+                    className={activeCategoryId === "button1" ? "active" : ""}
+                    onClick={() => handleClick("button1")}
                 >
                     Nike
                 </button>
-                <button>Adidas</button>
-                <button>Guci</button>
-                <button>MLB</button>
-                <button>Balenciaga</button>
-                <button>Puma</button>
+                <button
+                    id="button2"
+                    className={activeCategoryId === "button2" ? "active" : ""}
+                    onClick={() => handleClick("button2")}
+                >
+                    Adidas
+                </button>
+                <button
+                    id="button3"
+                    className={activeCategoryId === "button3" ? "active" : ""}
+                    onClick={() => handleClick("button3")}
+                >
+                    MLB
+                </button>
+                <button
+                    id="button4"
+                    className={activeCategoryId === "button4" ? "active" : ""}
+                    onClick={() => handleClick("button4")}
+                >
+                    Balenciaga
+                </button>
+                <button
+                    id="button5"
+                    className={activeCategoryId === "button5" ? "active" : ""}
+                    onClick={() => handleClick("button5")}
+                >
+                    Converse
+                </button>
                 <button className="sort">
                     Sắp Xếp <FiChevronDown />
                 </button>
