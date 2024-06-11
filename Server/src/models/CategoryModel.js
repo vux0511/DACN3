f164f958491c79337001cc246552b0e03b0518bb
phanvan2 <phanvanphung215@gmail.com>
2024-06-11 21:30:23 +0700

    order

--- Server/src/models/CategoryModel.js
+++ Server/src/models/CategoryModel.js
@@ -1,26 +1,34 @@
-import mongoose from "mongoose" ; 
+import mongoose from "mongoose";
 
-let Schema = mongoose.Schema; 
+let Schema = mongoose.Schema;
 
 let CategorySchema = new Schema({
-    nameCategory: {type: String, default: null},
-    idUser: {type:String, default: null},
-    imageCategory: {type:String, defautl: "default-category.png"},
-    createAt: {type: Number, default: Date.now},
-    updateAt:  {type: Number, default: Date.now},
-    deleteAt: {type: Number, default: null}
-})
+    nameCategory: { type: String, default: null },
+    idUser: { type: String, default: null },
+    imageCategory: { type: String, defautl: "default-category.png" },
+    createAt: { type: Number, default: Date.now },
+    updateAt: { type: Number, default: Date.now },
+    deleteAt: { type: Number, default: null },
+});
 
-CategorySchema.statics ={
-    createNew(item){
-        return this.create(item); 
+CategorySchema.statics = {
+    createNew(item) {
+        return this.create(item);
     },
-    getCategoryByName(nameCategory){
-        return this.findOne({nameCategory: nameCategory}).exec() ; 
+    getCategoryById(id) {
+        return this.findById(id).exec();
     },
-    getNormalCategoies(){
-        return this.find().exec() ; 
-    }
-}; 
+    getCategoryByName(nameCategory) {
+        return this.findOne({ nameCategory: nameCategory }).exec();
+    },
+    getNormalCategoies() {
+        return this.find().exec();
+    },
+    removeCategory(idUser, idCategory) {
+        return this.deleteOne({
+            $and: [{ idUser: idUser }, { _id: idCategory }],
+        }).exec();
+    },
+};
 
-export default mongoose.model("category", CategorySchema); 
+export default mongoose.model("category", CategorySchema);
--- Server/src/services/categoryService.js
+++ Server/src/services/categoryService.js
@@ -44,4 +44,26 @@ let getNormalCategoies = () => {
     });
 };
 
-export default { addNewCategory, getNormalCategoies, getCategoryByName };
+let removeCategory = (idUser, idCategory) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let result = await categoryModel.removeCategory(idUser, idCategory);
+            console.log(result);
+            if (result) {
+                resolve(result);
+            } else {
+                resolve(false);
+            }
+        } catch (error) {
+            console.log(error);
+
+            resolve(false);
+        }
+    });
+};
+export default {
+    addNewCategory,
+    getNormalCategoies,
+    getCategoryByName,
+    removeCategory,
+};
